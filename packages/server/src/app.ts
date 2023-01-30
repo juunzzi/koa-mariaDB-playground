92aa121aff214208e7f0334f9b3d70338e0ee6b5
juunzzi <78349600+juunzzi@users.noreply.github.com>
2023-01-30 10:25:37 +0900

    branch

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,4 +1,3 @@
-import { number_1 } from '@/ex';
 import Koa from 'koa';
 
 import logger from 'koa-logger';
@@ -9,9 +8,6 @@ app.use(logger());
 
 app.use(async (ctx) => {
   ctx.body = '준찌보';
-
-  console.log(number_1);
-  console.log('hi');
 });
 
 app.listen(3000);
